bbbee544be03f02883ee8a72c1e4499fa86417a4
System Administrator <root@Lorenas-MacBook-Pro.local>
2015-10-03 10:51:28 -0500

    Update repo with example solutions and README with instructions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gdi-js203
-Example answers for  GSI Chicago JS203 class.
+
+Example answers for the [GDI Chicago JavaScript 203 class](http://gdichicago.com/classes/js203/workshop/), repo [here](https://github.com/gdichicago/js203).
+
+Note - you will need to register for the [Big Thesaurus API](https://words.bighugelabs.com/api.php) and update the APIKey inside exercise3.js file.   
---
+++ exercise3.js
@@ -0,0 +1,54 @@
+
+$("#myWordButton").on('click', function(e) {
+    e.preventDefault();
+    $('#results').empty();
+    var myWord = $("#myWord").val();
+    console.log(myWord);
+    var apiUrl = "http://words.bighugelabs.com/api/2/<apiKeyFixMe>/";
+    var dataFormat = "/json";
+
+    if (myWord.length > 0) {
+        $.ajax({
+            type: 'GET',
+            dataType: 'jsonp',
+            url: apiUrl + myWord + dataFormat,
+            success: function(data) {
+                parseJsonData(data);
+            }
+        });
+    } else {
+        alert("You must enter a word!");
+    }
+
+});
+
+function parseJsonData(data) {
+    $.each(data, function(type, words) {
+        if (words.syn !== undefined) {
+            var synonyms = $("<div>");
+            var synonymsHeader = $('<h5>' + type + ' synonyms:</h5>');
+            var synonymsList = $('<ul>');
+            for (var i = 0; i < words.syn.length; i++) {
+                synonymsList.append('<li>' + words.syn[i] + '</li>');
+            }
+            synonymsList.append('</ul>');
+            synonyms.append(synonymsHeader);
+            synonyms.append(synonymsList);
+            synonyms.append("</div>");
+            $('#results').append(synonyms);
+        }
+        if (words.ant !== undefined) {
+            var antonyms = $("<div>");
+            var antonymsHeader = $('<h5>' + type + ' antonyms:</h5>');
+            var antonymsList = $('<ul>');
+            for (var idx = 0; idx < words.ant.length; idx++) {
+                antonymsList.append('<li>' + words.ant[idx] + '</li>');
+            }
+            antonymsList.append('</ul>');
+            antonyms.append(antonymsHeader);
+            antonyms.append(antonymsList);
+            antonyms.append("</div>");
+            $('#results').append(antonyms);
+        }
+    });
+}
